953b6e4564b08c94605f4f6be75b41ba45058eab
Patrik Bak <patrik.bak.x@gmail.com>
2020-11-13 16:15:00 +0100

    Alpha version

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,18 +1,22 @@
 ﻿using Generator.Equals;
 using System;
 
-[Equatable]
-partial record MyRecord(
-    [property: SequenceEquality] string[] Fruits
-);
-
-class Program
+namespace Test
 {
-    static void Main(string[] args)
-    {
-        var record1 = new MyRecord(new[] { "banana", "apple" });
-        var record2 = new MyRecord(new[] { "banana", "apple" });
+    [Equatable]
 
-        Console.WriteLine(record1 == record2);
+    partial record MyRecord(
+        [property: SequenceEquality] string[] Fruits
+    );
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var record1 = new MyRecord(new[] { "banana", "apple" });
+            var record2 = new MyRecord(new[] { "banana", "apple" });
+
+            Console.WriteLine(record1 == record2);
+        }
     }
 }
